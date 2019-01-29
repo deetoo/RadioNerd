b1bd52c45121c1a59b2d5b1e60ec6891fc88f876
deetoo <doug.dobies@gmail.com>
2019-01-29 13:52:45 -0600

    updated q.php

--- q.php
+++ q.php
@@ -1,57 +1,124 @@
 <html>
-<body bgcolor="#DDDDDD">
-<title>Search Results</title>
-<center>
-<table cellspacing=4>
-<tr>
-<th bgcolor="#EEEEEE">#</th><th bgcolor="#EEEEEE">Band/Artist</th><th bgcolor="#EEEEEE">Song Title</th><th bgcolor="EEEEEE">Play Date/Time<th>
-</tr>
-<tr>
+<head>
+<meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+        <link href="https://fonts.googleapis.com/css?family=Advent+Pro|Neucha|Oxygen|Saira+Extra+Condensed|Sedgwick+Ave|Yanone+Kaffeesatz" rel="stylesheet">`
+<style>
+
+body {
+background-color: #9099A2;
+margin: 0px;
+font-size: .8em; /* currently ems cause chrome bug misinterpreting rems on body element */
+  line-height: 1.6;
+  font-weight: 400;
+  font-family: "Raleway", "HelveticaNeue", "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+table {
+        font-family: 'Oxygen', sans-serif;
+        font-size: 1em;
+        padding: 6px;
+        border-spacing: 0.2rem;
+
+}
+
+td {
+        background-color: #8D097A0;
+
+}
+
+th {
+        font-family: 'Yanone Kaffeesatz', sans-serif;
+        font-size: 1.1em;
+        text-align: left;
+}
+
+a:link {
+        color: #FFEEEE;
+        text-decoration: none;
+        font-weight: bold;
+}
+
+a:hover {
+        color: #FFEEEE;
+        text-decoration: none;
+}
+
+a:visited {
+        color: #FFEEEE;
+        text-decoration: none;
+}
+
+a:active {
+        color: #FFEEEE;
+        text-decoration: none;
+}
+
+.container {
+  position: relative;
+  width: 100%;
+  max-width: 100%;
+  margin: 0 5px;
+  padding: 0 5px;
+  box-sizing: border-box;
+  display: flex;
+}
+
+.wrapper {
+max-width: 100%;
+margin: 40px auto 0 auto;
+line-height: 1.65;
+padding: 20px 50px;
+display: flex;
+}
+
+.FirstLast {
+        flex: 4;
+        background-color: #829295;
+        border: 1px solid #777777;
+        margin: 5;
+        padding: 3;
+}
+
+.LastTen {
+        flex: 2;
+        background-color: #829295;
+        border: 1px solid #777777;
+        margin: 5;
+        padding: 3;
+}
+
+.Dataz {
+        flex: 1;
+        background-color: #829295;
+        border: 1px solid #777777;
+        margin: 5;
+        padding: 3;
+}
+
+.Footer {
+        flex: 4;
+        background-color: #829295;
+        border: 1px solid #777777;
+        margin: 5;
+        padding:3;
+}
+</style>
+</head>
+
+<title>NerdRadio Stats</title>
+<body>
 <?php
-
-require("c.php");
-include("functions.php");
-
-$WhichBand = $_GET["band"];
-$WhichSong = $_GET["song"];
-
-
-$count="1";
-
-$mysqli = new mysqli('localhost', $uname, $pass, $db );
-
-        if( $mysqli->connect_error ) {
-        die('Connect error: ' . $mysqli->connect_errno . ' : ' . $mysqli->connect_error );
-        }
-
-	if (!empty($WhichBand) )
-        $sql = "SELECT * FROM ArtistData WHERE artistid=\"$WhichBand\"";
-
-	if (!empty($WhichSong) )
-	$sql = "SELECT * FROM SongData WHERE trackid=\"$WhichSong\"";
-
-        $query = $mysqli->query( $sql );
-
-        if( $query ) {
-                while( $row = mysqli_fetch_array( $query ) )
-                        {
-                        $band = $row['artistid'];
-                        $title = $row['trackid'];
-			$playtime = $row['playtime'];
-
-			$SongName = Translate("Song",$title);
-			$BandName = Translate("Band",$band);
-                print "<!-- start entry -->\n";
-		print "<tr>\n";
-                print "<td>$count</td><td><a href=\"q.php?band=$band\">$BandName</a></td>\n";
-		print "<td><a href=\"q.php?song=$title\">$SongName</a></td><td>$playtime</td>\n";
-                print "</tr>\n";
-                print "<!-- end entry -->\n\n";
-		$count++;
-                }
-	}
+include_once("functions.php");
 ?>
 
-</table>
-<a href="index.php">HOME</a>
-</center>
+
+<div class="wrapper">
+     <div class="FirstLast">
+
+     </div>
+</div>
+
+</body>
+</html>
